a9c112ea2a890ed6bc8e0eaf0fa88565b1e83ab2
Teo Klestrup Röijezon <teo@nullable.se>
2018-09-04 18:05:32 +0200

    drivesync: 1.3.1 -> 1.3.2

--- drivesync/default.nix
+++ drivesync/default.nix
@@ -1,12 +1,12 @@
 { stdenvNoCC, writeTextFile, fetchFromGitHub, callPackage }:
 let
-  version = "1.3.1";
+  version = "1.3.2";
   src = fetchFromGitHub {
     name = "drivesync-src-${version}";
     owner = "MStadlmeier";
     repo = "drivesync";
     rev = version;
-    sha256 = "0kyl1sqbbdk2jhx7vg9jzwb5a1m1i5c84hnv8hz8b4aw55mk3bmz";
+    sha256 = "1mal61pyhn8y7pbvwjd6a07yqybyf928s4ap1wk9lq69sx4hrh0k";
   };
   deps = callPackage ./deps.nix {};
 in writeTextFile {
